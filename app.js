0b084125531a086b1297a7fdc30879bc4566021c
Jaarabytes <128234882+Jaarabytes@users.noreply.github.com>
2023-05-30 22:57:59 +0300

    Update app.js
    jQuery FOR WHOOOOO!!!!!

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 $(document).ready(function() {
-    $("#subscribeForm").submit(function(event) {
-        event.preventDefault();
-    })
-    $("#submit").click(function() {
-      var email = $("#input").val();
-      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-      if (email === "" || !emailRegex.test(email)) {
-        $("#input").css({
-          "border": "1px solid red",
-          "color": "red",
-          "background-color":"pink"
-        });
-      } else {
-        $("#input").css({
-          "border": "none",
-          "color": "black"
-        });
-      }      
-    });
+  const form = $('#myForm');
+  // Add form submit event listener
+  form.on('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    var email = $("#input").val();
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "" || !emailRegex.test(email)) {
+      $("#input").css({
+        "border": "1px solid red",
+        "color": "red",
+        "background-color": "pink"
+      });
+      $("#label").text("Input a valid email");
+    } else {
+      $("#input").css({
+        "border": "1px solid gray",
+        "color": "black",
+        "background-color": "white"
+      });
+      $("#emailPlaceholder").text(email);
+      $('#popup').fadeIn();
+    }
   });
+
+  // Dismiss the popup when the "Dismiss message" button is clicked
+  $('#dismiss').on('click', function() {
+    $('#popup').fadeOut();
+  });
+});
